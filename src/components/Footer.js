0e938ca11bb58fb427108c2ab0b68bdc0f2877e9
ericksicard <esrfree@gmail.com>
2020-07-16 02:58:41 -0400

    Cleaning Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
